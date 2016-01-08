6b402599321e87afd8931733b6649b505dee427b
CJ Patoilo <cjpatoilo@gmail.com>
2016-01-08 10:20:30 -0300

    added meteor package support

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+	name: 'milligram:milligram-scss',
+	version: '0.0.1',
+	summary: 'A minimalist CSS framework on SCSS version.',
+	git: 'https://github.com/milligram/milligram-scss.git',
+	documentation: 'readme.md'
+});
+
+Package.onUse(function (api) {
+	api.versionsFrom('METEOR@1.0');
+	api.addFiles([
+		'dist/milligram.scss'
+	], 'client');
+});
